366c238f8adfbf19f3f51b8a3f46cfd2a8c5a9b2
rabirland <rabirland@gmail.com>
2024-11-21 14:51:07 +0100

    New Release

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,4 @@
+/* Manifest version: 9alEcY0Z */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
@@ -53,4 +54,3 @@ async function onFetch(event) {
 
     return cachedResponse || fetch(event.request);
 }
-/* Manifest version: tT/pgVsx */
